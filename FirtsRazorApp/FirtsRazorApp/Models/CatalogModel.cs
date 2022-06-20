e25395ffb04ed2071289b8c434f29b9dad1e65b4
HunnidStax <den4ik.trofimenko@gmail.com>
2022-06-20 19:10:04 +0300

    Start

---
+++ FirtsRazorApp/FirtsRazorApp/Models/CatalogModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FirtsRazorApp.Models
+{
+    public class CatalogModel
+    {
+        public List<Category> Categories { get; set; } = new();
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
