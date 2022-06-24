4c804fdc89b9281479c3d6c43ea4c7450d139f54
HunnidStax <den4ik.trofimenko@gmail.com>
2022-06-24 13:17:19 +0300

    Fixed

---
+++ FirstMVCApp/FirstMVCApp/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly Catalog _catalog;
+
+        public CatalogController(Catalog catalog)
+        {
+            _catalog = catalog;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Catalog"] = _catalog;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProductOnCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProductOnCreate([FromForm] Product product)
+        {
+            _catalog.ProductsList.Add(product);
+            return View();
+        }
+    }
+}
--- FirtsRazorApp/FirtsRazorApp/Models/ProductModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirtsRazorApp.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Img { get; set; }
-    }
-}
